6a1f661d26b1aae7eb1372b363ce433c2b081461
fredy016 <fredy016@outlook.com>
2022-07-16 16:55:08 -0600

    migración tabla autos
    Migración para crear tabla autos

---
+++ src/backend/database/migrations/2022_07_16_221503_create_autos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('autos', function (Blueprint $table) {
+            $table->id();
+            $table->string('placa', 20);
+            $table->unsignedBigInteger('id_tipo');
+            $table->timestamps();
+
+            // TODO Definir la llave foranea
+            $table->foreign('id_tipo')->references('id')->on('autos_tipo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('autos');
+    }
+}
