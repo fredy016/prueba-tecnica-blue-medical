0c86071a9e987884cd12e2759da4204c4561318c
fredy016 <fredy016@outlook.com>
2022-07-16 17:09:53 -0600

    Migración tabla estancias
    Migración para crear tabla estancias

---
+++ src/backend/database/migrations/2022_07_16_225733_create_estancias_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEstanciasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estancias', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_auto');
+            $table->unsignedBigInteger('id_user');
+            $table->timestamp('entrada');
+            $table->time('salida')->nullable();
+            $table->timestamps();
+
+            // TODO Definir las llaves foranea
+            $table->foreign('id_auto')->references('id')->on('autos');
+            $table->foreign('id_user')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estancias');
+    }
+}
