d6ca7c99bb1a86f67a67e6a329c2199de71a80e8
fredy016 <fredy016@outlook.com>
2022-07-16 22:41:26 -0600

    Migración estancia
    Definir en la instancia el campo Entrada con un valor por defecto CURRENT_TIMESTAMP

--- src/backend/database/migrations/2022_07_16_225733_create_estancias_table.php
+++ src/backend/database/migrations/2022_07_16_225733_create_estancias_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateEstanciasTable extends Migration
@@ -17,7 +18,7 @@ class CreateEstanciasTable extends Migration
             $table->id();
             $table->unsignedBigInteger('id_auto');
             $table->unsignedBigInteger('id_user');
-            $table->timestamp('entrada');
+            $table->timestamp('entrada')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->time('salida')->nullable();
             $table->timestamps();
 
@@ -34,7 +35,7 @@ class CreateEstanciasTable extends Migration
      */
     public function down()
     {
-        Schema::table('estancias', function (Blueprint $table){
+        Schema::table('estancias', function (Blueprint $table) {
             $table->dropForeign('estancias_id_auto_foreign');
             $table->dropForeign('estancias_id_user_foreign');
         });
