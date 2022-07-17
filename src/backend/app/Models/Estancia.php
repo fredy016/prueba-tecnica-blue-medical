030af70fea73a5e4d05478038d94fd51104ec6ba
fredy016 <fredy016@outlook.com>
2022-07-17 14:09:36 -0600

    Modelo Estancia
    Agregar función para calcular la cantidad de minutos de estacionamiento

--- src/backend/app/Models/Estancia.php
+++ src/backend/app/Models/Estancia.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,5 +17,15 @@ class Estancia extends Model
 
     public function auto(){
         return $this->belongsTo(Auto::class, 'id');
+    }
+
+    public function getDiffInDaysAttribute()
+    {
+        if (!empty($this->entrada) && !empty($this->salida)) {
+            $to = Carbon::createFromFormat('Y-m-d H:i:s', $this->entrada);
+            $from = Carbon::createFromFormat('Y-m-d H:i:s', $this->salida);
+            return $to->diffInMinutes($from);
+        }
+        return 0;
     }
 }
