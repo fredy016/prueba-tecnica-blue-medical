edd8cb00e8c47af298dfacbe67907301e67ff913
fredy016 <fredy016@outlook.com>
2022-07-17 23:13:19 -0600

    Crear Modulo Comenzar Mes

---
+++ src/frontend/src/app/components/comenzar-mes/comenzar-mes.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ComenzarMesRoutingModule } from './comenzar-mes-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ComenzarMesRoutingModule
+  ]
+})
+export class ComenzarMesModule { }
