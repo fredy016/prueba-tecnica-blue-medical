a447c375c864243ab0008f2d6f13fc171e81d737
fredy016 <fredy016@outlook.com>
2022-07-18 01:31:29 -0600

    Formato informe pago residentes

--- src/backend/routes/web.php
+++ src/backend/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Barryvdh\DomPDF\PDF;
+
+use App\Models\Auto;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +17,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/{nombrearchivo}', function ($nombrearchivo) {
+
+    $pdf = app('dompdf.wrapper');
+
+    /*******************************Cuerpo del Reporte en HTML*******************************/
+    $autos = Auto::where('id_tipo', 2)->where('tiempo_estacionado', '>', 0)->get();
+    $filas = '<tr><td>Núm. placa</td><td>Tiempo estacionado (min.)</td><td align="right">Cantidad a pagar</td></tr>';
+    foreach ($autos as $auto) {
+        $filas .= '<tr><td>' . $auto->placa . '</td><td>'.$auto->tiempo_estacionado.'</td><td align="right">'.number_format($auto->tiempo_estacionado * 0.05, 2).'</td></tr>';
+    }
+
+    $html = '
+    <html>
+        <body>
+            <table width="100%">
+                ' . $filas . '
+            </table>
+        </body>
+    </html>';
+
+
+    /*******************************Cuerpo del Reporte en HTML*******************************/
+
+    $pdf = $pdf->loadHTML($html);
+//
+//    return $pdf->stream($nombrearchivo.'.pdf');
+    return $pdf->download($nombrearchivo.'.pdf');
 });
