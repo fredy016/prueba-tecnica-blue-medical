4231901d70bfadb0fa2e22dd0ab148b68a10d588
fredy016 <fredy016@outlook.com>
2022-07-18 02:59:01 -0600

    CORS - Microservicio Angular
    Permitir la conexión a la ruta del microservicio en el puerto 8080 y a la ruta de angular en desarrollo

--- src/backend/config/cors.php
+++ src/backend/config/cors.php
@@ -3,7 +3,7 @@ return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
     // 'allowed_origins' => ['http://127.0.0.1:8080/', 'http://localhost:8080/'], <-- doesn't work, still gets CORS error
-    'allowed_origins' => ['http://localhost:4200'],  // <-- it works but it should not be like that
+    'allowed_origins' => ['http://localhost:4200', 'http://localhost:8080'],  // <-- it works but it should not be like that
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     // 'allowed_headers' => ['*'],
